0f8ba0a6fc76f1551045efed7244310db4680985
Sean J. Smith <sjsmith5@student.fullsail.edu>
2025-04-19 22:41:58 -0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'neon-green': '#39FF14',
+        'steel-gray': '#1c1f26',
+      },
+    },
+  },
+  plugins: [],
+};
