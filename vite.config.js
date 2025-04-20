0fb6abb3bdfecaa8f48c2eb4ef40ade813dc36e3
Sean J. Smith <sjsmith5@student.fullsail.edu>
2025-04-19 22:55:38 -0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Replace 'your-repo-name' with your actual repo name (case-sensitive!)
-export default defineConfig({
-  base: '/lock-in-portal/', 
-  plugins: [react()]
-})
